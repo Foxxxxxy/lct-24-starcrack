c1df8f062320f42d14ebb92bbcacebbc92dfa640
maria <astakhova.mary@gmail.com>
2024-06-08 16:03:48 +0300

    add events table

---
+++ app/db/events.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, JSON, Enum, DateTime
+
+from sqlalchemy.sql import func
+from db.base import Base
+
+
+class EventType(Enum):
+    DYNAMIC_SCHEDULE_REQUEST = 'DYNAMIC_SCHEDULE_REQUEST'
+    UNSCHEDULED_REQUEST = 'UNSCHEDULED_REQUEST'
+
+
+class Criticality(Enum):
+    CRITICAL = 'CRITICAL'
+    WARNING = 'WARNING'
+    NORMAL = 'NORMAL'
+
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(Enum(EventType), nullable=False)
+    data = Column(JSON, nullable=False)
+    criticality = Column(Enum(Criticality), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
