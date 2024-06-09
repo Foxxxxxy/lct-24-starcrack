9cef8074b29b71810f22aada786e0d2ad84afcbd
maria <astakhova.mary@gmail.com>
2024-06-09 18:15:51 +0300

    sqlalchemy models fix

--- app/db/models/employees.py
+++ app/db/models/employees.py
@@ -1,5 +1,25 @@
+import enum
+
 from sqlalchemy import Column, Integer, String, Enum
 from db.base import Base
+
+
+class SexType(enum.Enum):
+    male = 'male'
+    female = 'female'
+
+
+class RoleType(enum.ENUM):
+    Admin = 'Admin'
+    Attendant = 'Attendant'
+    Operator = 'Operator'
+    Specialist = 'Specialist'
+
+
+class SubRoleType(enum.Enum):
+    Head_of_the_section = 'Head of the section'
+    Senior_inspector = 'Senior inspector'
+    Inspector = 'Inspector'
 
 
 class Employee(Base):
@@ -7,6 +27,6 @@ class Employee(Base):
 
     id = Column(Integer, primary_key=True)
     full_name = Column(String(255), nullable=False)
-    sex = Column(Enum('Male', 'Female', name='gender_types'), nullable=False)
-    role = Column(String(255), nullable=False)
-    sub_role = Column(String(255), nullable=False)
+    sex = Column(Enum(SexType), nullable=False)
+    role = Column(Enum(RoleType), nullable=False)
+    sub_role = Column(Enum(SubRoleType), nullable=False)
