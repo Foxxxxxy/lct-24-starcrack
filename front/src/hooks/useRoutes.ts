149eb03b51a4dac00bcab3b4b5b2bc5209806b4b
LehaRybkoha <vintuvintu34@gmail.com>
2024-06-14 22:48:14 +0300

    inited new page

--- front/src/hooks/useRoutes.ts
+++ front/src/hooks/useRoutes.ts
@@ -1,7 +1,6 @@
 import {LayoutMain} from 'src/Layouts/LayoutMain';
 import {EmoloyeePage} from 'src/pages/EmoloyeePage';
 import {MainPage} from 'src/pages/MainPage';
-import {PassengersPage} from 'src/pages/PassengersPage';
 import {WorkTimePage} from 'src/pages/WorkTimePage';
 
 export const routes = [
@@ -14,6 +13,18 @@ export const routes = [
         path: '/passengers',
         LayoutComponent: LayoutMain,
         PageComponent: PassengersPage,
+    },
+    {
+        name: 'employeeCreate',
+        path: '/employee/create',
+        LayoutComponent: LayoutMain,
+        PageComponent: EmoloyeePage,
+    },
+    {
+        name: 'workTimeCreate',
+        path: '/work-time/create',
+        LayoutComponent: LayoutMain,
+        PageComponent: WorkTimePage,
     },
     {
         name: 'employeeCreate',
