d998ffc700d65e72abe1d847471c9a9c6806269b
Alexey <alexm20games@gmail.com>
2024-06-16 01:41:42 +0300

    upd LayoutEmpty, created FooterItems component

--- front/src/Layouts/LayoutEmpty.tsx
+++ front/src/Layouts/LayoutEmpty.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import {PageLayout} from '@gravity-ui/navigation';
-
 type LayoutProps = {
     children?: React.ReactNode;
 };
 
 export const LayoutEmpty: React.FC<LayoutProps> = ({children}) => {
     return (
-        <PageLayout.Content>{children}</PageLayout.Content>
+        <>{children}</>
     );
 };
--- front/src/Layouts/LayoutMain.tsx
+++ front/src/Layouts/LayoutMain.tsx
@@ -4,6 +4,7 @@ import {PageLayout, PageLayoutAside, FooterItem} from '@gravity-ui/navigation';
 import {useMenuItems} from 'src/hooks/useMenuItems';
 import {Avatar} from '@gravity-ui/uikit';
 import {SidebarPopup} from 'src/components/SidebarPopup/SidebarPopup';
+import {FooterItems} from 'src/components/FooterItems/FooterItems';
 
 import metroLogo from './vite.svg';
 
@@ -28,34 +29,7 @@ export const LayoutMain: React.FC<LayoutProps> = ({children}) => {
                 }}
                 onChangeCompact={(val) => setIsCompact(val)}
                 renderFooter={({compact}) => {
-                    const footerItems = [
-                        <FooterItem 
-                            key="1"
-                            compact={compact}
-                            item={{
-                                id: 'account',
-                                title: 'Учётная запись',
-                                itemWrapper: ({title}: any, makeItem: any) => {
-                                    return makeItem({
-                                        title,
-                                        icon: (
-                                            <Avatar imgUrl="https://lh3.googleusercontent.com/ogw/AF2bZyjQPFn3Mvwvp3JNzQqP7Xo_V9jGARrJ8RHSAxCG_09c7Ac=s64-c-mo" size="m" />
-                                        ),
-                                    });
-                                },
-                                onItemClick: () => {setPopupVisible(!popupVisible);},
-                            }}
-                            enableTooltip={!popupVisible}
-                            popupVisible={popupVisible}
-                            onClosePopup={() => setPopupVisible(false)}
-                            renderPopupContent={() => {
-                                return (
-                                    <SidebarPopup username='Рыбко Алексей Александрович' status='Администратор' />
-                                )
-                            }}
-                        />,
-                    ];
-                    return <>{footerItems}</>
+                    return <FooterItems compact={compact} />;
                 }}
             />
 
